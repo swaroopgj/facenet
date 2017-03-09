7e8ddf2ba4481085ad002f090da2d9799254e0cb
Swaroop Guntupalli <swaroopgj@gmail.com>
2017-03-08 21:11:36 -0500

    Hacks to make aligne work for me

--- src/align/align_dataset_mtcnn.py
+++ src/align/align_dataset_mtcnn.py
@@ -32,7 +32,8 @@ import argparse
 import tensorflow as tf
 import numpy as np
 import facenet
-import align.detect_face
+#import align.detect_face
+import detect_face
 import random
 
 def main(args):
@@ -51,7 +52,8 @@ def main(args):
         gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=args.gpu_memory_fraction)
         sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
         with sess.as_default():
-            pnet, rnet, onet = align.detect_face.create_mtcnn(sess, '../../data/')
+            #pnet, rnet, onet = align.detect_face.create_mtcnn(sess, '../../data/')
+            pnet, rnet, onet = detect_face.create_mtcnn(sess, '../../data/')
     
     minsize = 20 # minimum size of face
     threshold = [ 0.6, 0.7, 0.7 ]  # three steps's threshold
@@ -92,7 +94,8 @@ def main(args):
                             img = facenet.to_rgb(img)
                         img = img[:,:,0:3]
     
-                        bounding_boxes, _ = align.detect_face.detect_face(img, minsize, pnet, rnet, onet, threshold, factor)
+                        #bounding_boxes, _ = align.detect_face.detect_face(img, minsize, pnet, rnet, onet, threshold, factor)
+                        bounding_boxes, _ = detect_face.detect_face(img, minsize, pnet, rnet, onet, threshold, factor)
                         nrof_faces = bounding_boxes.shape[0]
                         if nrof_faces>0:
                             det = bounding_boxes[:,0:4]
